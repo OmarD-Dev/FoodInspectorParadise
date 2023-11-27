32a670713d29504b24eb99f2fc0a6d010d5de720
Annie Santamaria <smaria.annie@gmail.com>
2023-11-27 10:56:30 -0500

    Test commit

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class Order {
     sandwich sandwich;
-    ArrayList<sandwich> sandwiches = new ArrayList<>(); // all the sandwhiches in the oder... we can do this for the other stuff too later
-    boolean drink; // we can make it alot more detailed later but for now drink is either yes or no
-    boolean chips;// we can make it alot more detailed later but for now chips are either yes or no
+    ArrayList<sandwich> sandwiches = new ArrayList<>(); // all the sandwiches in the oder... we can do this for the other stuff too later
+    boolean drink; // we can make it a lot more detailed later but for now drink is either yes or no
+    boolean chips;// we can make it a lot more detailed later but for now chips are either yes or no
 
 }
