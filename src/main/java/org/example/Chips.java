59cd6539e652986d8d1ea28d170912f6d079e8a9
Annie Santamaria <smaria.annie@gmail.com>
2023-11-29 20:43:14 -0500

    Reverted drinks class back to enums

--- src/main/java/org/example/Chips.java
+++ src/main/java/org/example/Chips.java
@@ -2,7 +2,7 @@ package org.example;
 
 public enum Chips { LAYS, FRITOS, CHEETOS, KETTLE, DORITOS, FUNYUNS;
 
-    public static final double cost = 1.50;
+    public static final double chipCost = 1.50;
     //enum for type of chips
     // we can also implement it as an int for size since the user isn't directly typing into the program it's up to us to store it
 
--- src/main/java/org/example/Drinks.java
+++ src/main/java/org/example/Drinks.java
@@ -1,27 +1,8 @@
 package org.example;
 
-public class Drinks {
-    private ArrayList<String> drinkType;
+public enum Drinks { FANTA, COLA, PEPSI, SPRITE
 
-    public Drinks() {
-        this.drinkType = new ArrayList<>();
-        initializeDrinks();
-    }
-
-    private void initializeDrinks() {
-        drinkType.add("Coca Cola");
-        drinkType.add("Pepsi");
-        drinkType.add("Fanta");
-        drinkType.add("Sprite");
-        drinkType.add("Mountain Dew");
-        drinkType.add("Dr. Pepper");
-    }
-
-    /*
-    NOTE: how will add flavor relate to the order screen?
-                might need to come back to this method
-     */
-    public void addDrink(String flavor, DrinkSize size) {
-        this.drinkType.add(flavor);
-    }
+//    public void addDrink(String flavor, DrinkSize size) {
+//        this.drinkType.add(flavor);
+//    }
 }
